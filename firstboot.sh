f0bfe0b5ae93bc661635f88dbbe05d737eb26637
br8e <br8e@users.noreply.github.com>
2017-04-08 20:48:27 +0100

    Committer: br8e <br8e@users.noreply.github.com>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   firstboot.sh 	new file:   grubtheme.sh  Changes not staged for commit: 	deleted:    grubinstall.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,24 +1,15 @@
  
 #! /bin/bash
-echo starting Kali upgrades...
-
-
-#! Update, Upgrade, Dist-Upgrade
-echo updating system...
 apt-get clean && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
-echo done.
 
 #! Installs applications
-echo installing applications...
 apt-get install dirmngr -y
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 apt-get update
 apt-get install alsa-utils unrar unace rar unrar p7zip zip unzip p7zip-full p7zip-rar file-roller software-properties-common apt-file gnome-themes-standard tor torbrowser-launcher filezilla filezilla-common gtk-recordmydesktop recordmydesktop remmina gdebi uget x2goclient numlockx smplayer spotify-client -y
-echo done.
 
 #! add debian repository
-echo adding debian repository
 apt-file update
 apt-file search add-apt-repository
 cd /usr/sbin
@@ -52,20 +43,22 @@ cp test.txt /usr/sbin/add-apt-repository
 rm test.txt
 chmod o+x /usr/sbin/add-apt-repository
 chown root:root /usr/sbin/add-apt-repository
-echo done.
 
-#! downloads themes
-echo downloading Themes...
+#!update keys
+cp /etc/apt/sources.list /etc/apt/sources.list.bk
 /usr/sbin/add-apt-repository ppa:noobslab/themes
-echo done.
+rm /etc/apt/sources.list
+mv /etc/apt/sources.list.bk /etc/apt/sources.list
 
 #!arc kde
-echo downloading arc...
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/arc-kde/master/install-arc-kde-root.sh | sh
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-smplayer-theme/master/install-papirus-root.sh | sh
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-filezilla-themes/master/install-papirus-root.sh | sh
 
-echo script complete please restart
-echo type reboot and press enter to reboot now or just type exit to close
-read xit
-$xit
+#!grubtheme
+chmod +x ./grubtheme.sh
+./grubtheme.sh
+
+#!grub-customizer
+chmod +x ./grubcustomizer.sh
+./grubcustomizer.sh
